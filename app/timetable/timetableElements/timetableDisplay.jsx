ef0cd1581d0da61bd28da7e9cd4fc23ea4ba6e76
konryu-kin <konryu.kin27@gmail.com>
2025-06-12 10:48:45 +0000

    ・タイムテーブル関連を整備 ・day1かday2かを選択するボタンの設置

---
+++ app/timetable/timetableElements/timetableDisplay.jsx
@@ -0,0 +1,18 @@
+"use client";
+import { useState } from "react";
+import { ToggleButton } from "./timetable_toggleButton.jsx"
+
+export function TimetableDisplay(){
+    const now = new Date()
+    const [day,setDay] = useState((now.getMonth()==8 && now.getDay()==7) ? 2 : 1);
+    
+    function toggleValue(){
+        setDay(3 - day);
+    }
+
+    return(
+        <>
+            <ToggleButton day={day} onClick={toggleValue}/>
+        </>
+    )
+}
