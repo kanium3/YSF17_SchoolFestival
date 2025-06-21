b212f8f8ebb5924fe2ffbdeee57f56e5c3116ccc
ameirok <151885371+proirok@users.noreply.github.com>
2025-06-21 02:50:08 +0000

    サンプルの作成が一旦終了！

--- apps/web/app/map/map.jsx
+++ apps/web/app/map/map.jsx
@@ -1,13 +1,13 @@
 'use client'
 import { CRS, LatLng, LatLngBounds, icon } from 'leaflet'
 import React, { useState } from 'react'
-import { MapContainer, ImageOverlay, LayersControl, Marker, Popup, useMapEvents } from 'react-leaflet'
+import { MapContainer, ImageOverlay, LayersControl, Marker, Popup, useMapEvents, LayerGroup } from 'react-leaflet'
 import Link from 'next/link'
 import Image from 'next/image'
 import './map.css'
 import 'leaflet/dist/leaflet.css' // リーフレットの本体のCSSの読み込み(これしないと地図が崩れる)
 
-const { BaseLayer } = LayersControl // 地図関連の値はLayersControlに入っているためそれを変数として定義
+/*const { BaseLayer } = LayersControl // 地図関連の値はLayersControlに入っているためそれを変数として定義
 
 const layers = [{ name: '1階', url: 'https://raw.githubusercontent.com/proirok/buffer/refs/heads/main/sample_map.svg' },
   { name: '2階', url: 'https://raw.githubusercontent.com/proirok/buffer/refs/heads/main/sample_map2.svg' }]
@@ -63,7 +63,50 @@ export default function Map() {
       <Control setlayers={setlayers} />
     </MapContainer>
   )
-};
+};*/
+
+export default function Map(){
+  const picwidth = 960
+  const picheight = 540
+  const position1 = [picheight / 2, picwidth / 2]
+
+  const ICON = icon({
+    iconUrl: 'https://raw.githubusercontent.com/proirok/buffer/refs/heads/main/map_pin.svg',
+    iconSize: [200, 200],
+    iconAnchor: [100, 100],
+    popupAnchor: [0, -50],
+  })
+  return (
+    <MapContainer
+      crs={CRS.Simple}
+      center={new LatLng(picheight / 2, picwidth / 2)}
+      zoom={0}
+      style={{ width: picwidth, height: picheight }}
+      maxBounds={[[0, 0], [picheight, picwidth]]}
+    >
+      <LayersControl position="bottomright" collapsed="false">
+        <LayersControl.BaseLayer checked name='1階'>
+          <LayerGroup>
+            <ImageOverlay url={'https://raw.githubusercontent.com/proirok/buffer/refs/heads/main/sample_map.svg'} bounds={new LatLngBounds([[0,0], [picheight, picwidth]])}/>
+          </LayerGroup>
+        </LayersControl.BaseLayer>
+        <LayersControl.BaseLayer name='2階'>
+          <LayerGroup>
+            <ImageOverlay url={'https://raw.githubusercontent.com/proirok/buffer/refs/heads/main/sample_map2.svg'} bounds={new LatLngBounds([[0,0], [picheight, picwidth]])}/>
+            <Marker position={position1} icon={ICON}>
+            <Popup>
+              <Image src="https://raw.githubusercontent.com/proirok/buffer/refs/heads/main/map_pin.svg" alt="マップピンアイコン画像" width={200} height={100} />
+              <Link href="https://www.mrchildren.jp/" target="blank" rel="noopener noreferrer">
+                Mr.Childrenのホームページ
+              </Link>
+            </Popup>
+          </Marker>
+          </LayerGroup>
+        </LayersControl.BaseLayer>
+      </LayersControl>
+    </MapContainer>
+    )
+}
 
 /*
 その他技術的なコメント
