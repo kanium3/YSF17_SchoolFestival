3326319693c24d534894b8f72d34144217ccb200
OR-11 <93813653+OR-11@users.noreply.github.com>
2025-06-21 23:20:39 +0900

    コメントアウトにスペースを追加

--- apps/web/app/dining/foodSales/foodSalesElements/food-sales-allergies-filter.jsx
+++ apps/web/app/dining/foodSales/foodSalesElements/food-sales-allergies-filter.jsx
@@ -5,7 +5,7 @@ import { FoodSalesCardExporter } from './food-sale-card-exporter';
 export function FoodSalesAllergiesFilter(_allergies = []) {
   const Cards = foodSalesMenu().map(item => <FoodSalesCardExporter cardData={item}/>);
   return (
-    //とりあえずそのまま横流し
+    // とりあえずそのまま横流し
     <div>
       {/** アレルギー物質を「含まない」やつ */}
       <div id='foodSaleMenus' className={styles.foodSalesMenuArea}>
