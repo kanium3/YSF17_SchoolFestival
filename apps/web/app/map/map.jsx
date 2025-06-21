0ec7d71252bbc742e66bee93e54cf0445f445fb8
ameirok <151885371+proirok@users.noreply.github.com>
2025-06-21 03:10:39 +0000

    Eslintに引っかかった部分を修正しました。

--- apps/web/app/map/map.jsx
+++ apps/web/app/map/map.jsx
@@ -1,13 +1,13 @@
 'use client'
 import { CRS, LatLng, LatLngBounds, icon } from 'leaflet'
-import React, { useState } from 'react'
-import { MapContainer, ImageOverlay, LayersControl, Marker, Popup, useMapEvents, LayerGroup } from 'react-leaflet'
+import React from 'react'
+import { MapContainer, ImageOverlay, LayersControl, Marker, Popup, LayerGroup } from 'react-leaflet'
 import Link from 'next/link'
 import Image from 'next/image'
 import './map.css'
 import 'leaflet/dist/leaflet.css' // リーフレットの本体のCSSの読み込み(これしないと地図が崩れる)
 
-export default function Map(){
+export default function Map() {
   const picwidth = 960
   const picheight = 540
   const position1 = [picheight / 2, picwidth / 2]
@@ -27,25 +27,25 @@ export default function Map(){
       maxBounds={[[0, 0], [picheight, picwidth]]}
     >
       <LayersControl position="bottomright" collapsed="false">
-        <LayersControl.BaseLayer checked name='1階'>
+        <LayersControl.BaseLayer checked name="1階">
           <LayerGroup>
-            <ImageOverlay url={'https://raw.githubusercontent.com/proirok/buffer/refs/heads/main/sample_map.svg'} bounds={new LatLngBounds([[0,0], [picheight, picwidth]])}/>
+            <ImageOverlay url="https://raw.githubusercontent.com/proirok/buffer/refs/heads/main/sample_map.svg" bounds={new LatLngBounds([[0, 0], [picheight, picwidth]])} />
           </LayerGroup>
         </LayersControl.BaseLayer>
-        <LayersControl.BaseLayer name='2階'>
+        <LayersControl.BaseLayer name="2階">
           <LayerGroup>
-            <ImageOverlay url={'https://raw.githubusercontent.com/proirok/buffer/refs/heads/main/sample_map2.svg'} bounds={new LatLngBounds([[0,0], [picheight, picwidth]])}/>
+            <ImageOverlay url="https://raw.githubusercontent.com/proirok/buffer/refs/heads/main/sample_map2.svg" bounds={new LatLngBounds([[0, 0], [picheight, picwidth]])} />
             <Marker position={position1} icon={ICON}>
-            <Popup>
-              <Image src="https://raw.githubusercontent.com/proirok/buffer/refs/heads/main/map_pin.svg" alt="マップピンアイコン画像" width={200} height={100} />
-              <Link href="https://www.mrchildren.jp/" target="blank" rel="noopener noreferrer">
-                Mr.Childrenのホームページ
-              </Link>
-            </Popup>
-          </Marker>
+              <Popup>
+                <Image src="https://raw.githubusercontent.com/proirok/buffer/refs/heads/main/map_pin.svg" alt="マップピンアイコン画像" width={200} height={100} />
+                <Link href="https://www.mrchildren.jp/" target="blank" rel="noopener noreferrer">
+                  Mr.Childrenのホームページ
+                </Link>
+              </Popup>
+            </Marker>
           </LayerGroup>
         </LayersControl.BaseLayer>
       </LayersControl>
     </MapContainer>
-    )
+  )
 }
