395815276809a4f971fb73c32d384410dcb0d390
OR-11 <93813653+OR-11@users.noreply.github.com>
2025-06-21 23:15:43 +0900

    このファイルが変に改変されてエラーを吐いていたので修正

--- apps/web/app/dining/foodSales/foodSalesElements/food-sales-allergies-filter.jsx
+++ apps/web/app/dining/foodSales/foodSalesElements/food-sales-allergies-filter.jsx
@@ -1,21 +1,19 @@
-import styles from '../page.module.css'
-import { foodSalesMenu } from './food-sales-menus.js'
-import { FoodSalesCardExporter } from './food-sale-card-exporter.jsx'
+import styles from "../page.module.css";
+import { foodSalesMenu } from "./food-sales-menus";
+import { FoodSalesCardExporter } from "./food-sale-card-exporter";
 
-export function FoodSalesAllergiesFilter({ _allergies = [] }) {
-  const saleMenu = foodSalesMenu()
+export function FoodSalesAllergiesFilter(allergies = []) {
+  const Cards = foodSalesMenu().map(item => <FoodSalesCardExporter cardData={item}/>);
   return (
-    // とりあえずそのまま横流し
+    //とりあえずそのまま横流し
     <div>
       {/** アレルギー物質を「含まない」やつ */}
       <div id="foodSaleMenus" className={styles.foodSalesMenuArea}>
-        {saleMenu.map((item) => {
-          return <FoodSalesCardExporter name={item.name} team={item.team} menus={item.menus} teamId={item.teamId} key={item.name} />
-        })}
+        { Cards }
       </div>
       {/** 下はアレルギー物質を「含む」やつ */}
       <div id="foodSaleMenus" className={styles.foodSalesMenuArea}>
-
+        
       </div>
     </div>
   )
