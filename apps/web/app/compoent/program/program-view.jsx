29d81ae9bdd985e6787c459d0f606fffbb8b1939
kanium3 <dev@kanium.me>
2025-06-22 12:50:18 +0900

    階に応じたcardの表示

--- apps/web/app/compoent/program/program-view.jsx
+++ apps/web/app/compoent/program/program-view.jsx
@@ -2,61 +2,83 @@ import styles from './program-view.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
 
+const ariaOrder = ['1F', '2F', '3F', '4F', '5F', '屋上', '体育館', '交流センター']
+
+function groupArray(array) {
+  const groups = {}
+  for (const item of array) {
+    if (!groups[item.aria]) {
+      groups[item.aria] = []
+    }
+    groups[item.aria].push(item)
+  }
+  return Object.entries(groups).map(([aria, item]) => ({ aria, item }))
+}
+
 /**
  *
  * @param {import("@latimeria/core").Programs} programs
  * @constructor
  */
 export default function ProgramView({ programs }) {
-  const programsArray = [...programs.iter()]
+  const programsArray = [...programs.iter()].sort((a, b) => ariaOrder.indexOf(a.aria) - ariaOrder.indexOf(b.aria))
+  /** @type {[{aria:string , item:Program[]}]} */
+  const ariaGroups = groupArray(programsArray)
   return (
     <div className={styles.list}>
-      <ol>
-        {programsArray.map((program) => {
-          return (
-            <li key={program.id}>
-              <Link href={`/program/${program.id}`}>
-                <div className={styles.card}>
-                  <div className={styles.cardTitle}>
-                    <div className={styles.imageInCard}>
-                      <Image src="/latimeria_logo.png" alt="仮企画ロゴです" fill={true} />
-                    </div>
-                    <div className={styles.cardDescription}>
-                      <h2>{program.name}</h2>
-                      <p className={styles.cardPRText}>{program.prText || ''}</p>
-                      <div className={styles.cardTags}>
-                        {program.optionalTag === undefined
-                          ? ''
-                          : program.optionalTag.map(tag => (
-                              <span key={tag} className={styles.cardTag}>
-                                #
-                                {tag}
-                              </span>
-                            ))}
+      {ariaGroups.map(({ aria, item }, index) => {
+        return (
+          <div key={`${aria}-${index}`}>
+            <h2 key={`${aria}-${index}`}>{aria || ''}</h2>
+            <ol>
+              {item.map((program) => {
+                return (
+                  <li key={program.id}>
+                    <Link href={`/program/${program.id}`}>
+                      <div className={styles.card}>
+                        <div className={styles.cardTitle}>
+                          <div className={styles.imageInCard}>
+                            <Image src="/latimeria_logo.png" alt="仮企画ロゴです" fill={true} />
+                          </div>
+                          <div className={styles.cardDescription}>
+                            <h2>{program.name}</h2>
+                            <p className={styles.cardPRText}>{program.prText || ''}</p>
+                            <div className={styles.cardTags}>
+                              {program.optionalTag === undefined
+                                ? ''
+                                : program.optionalTag.map(tag => (
+                                    <span key={tag} className={styles.cardTag}>
+                                      #
+                                      {tag}
+                                    </span>
+                                  ))}
+                            </div>
+                          </div>
+                        </div>
+                        <div className={styles.cardPlaceAndType}>
+                          <p>
+                            {program.aria}
+                            @
+                            {program.location}
+                          </p>
+                          {program.programType.map((programType) => {
+                            const [backgroundColor, textColor] = backgroundAndTextColor(programType)
+                            const textStyle = {
+                              background: backgroundColor,
+                              color: textColor,
+                            }
+                            return <span key={programType} className={styles.cardProgramType} style={textStyle}>{programType}</span>
+                          })}
+                        </div>
                       </div>
-                    </div>
-                  </div>
-                  <div className={styles.cardPlaceAndType}>
-                    <p>
-                      {program.aria}
-                      @
-                      {program.location}
-                    </p>
-                    {program.programType.map((programType) => {
-                      const [backgroundColor, textColor] = backgroundAndTextColor(programType)
-                      const textStyle = {
-                        background: backgroundColor,
-                        color: textColor,
-                      }
-                      return <span key={programType} className={styles.cardProgramType} style={textStyle}>{programType}</span>
-                    })}
-                  </div>
-                </div>
-              </Link>
-            </li>
-          )
-        })}
-      </ol>
+                    </Link>
+                  </li>
+                )
+              })}
+            </ol>
+          </div>
+        )
+      })}
     </div>
   )
 }
