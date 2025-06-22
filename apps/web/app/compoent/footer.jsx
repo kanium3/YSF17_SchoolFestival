71121e1025af1d8a498e42cdbe9898df05363f35
ysy-255 <ysy-255@outlook.com>
2025-06-22 16:28:28 +0900

    こう書いたほうがいいね

--- apps/web/app/compoent/footer.jsx
+++ apps/web/app/compoent/footer.jsx
@@ -5,25 +5,22 @@ import styles from "./footer.module.css"
 import Link from "next/link"
 import { MdHome, MdOutlineFastfood, MdWysiwyg, MdAccessTime, MdOutlineMap } from "react-icons/md"
 
-function createIcon(Icon) {
-  return <Icon size={40} className={styles["ft-icon"]} />;
-}
 
 export default function Footer(){
   const [activePage, setActivePage] = useState(null);
   const items = [
-    { label: "案内",   href: "/",          icon: MdHome },
-    { label: "食品",   href: "/dining",    icon: MdOutlineFastfood },
-    { label: "企画",   href: "/program",   icon: MdWysiwyg },
-    { label: "時間割", href: "/timetable", icon: MdAccessTime },
-    { label: "地図",   href: "/map",       icon: MdOutlineMap }
+    { label: "案内",   href: "/",          Icon: MdHome },
+    { label: "食品",   href: "/dining",    Icon: MdOutlineFastfood },
+    { label: "企画",   href: "/program",   Icon: MdWysiwyg },
+    { label: "時間割", href: "/timetable", Icon: MdAccessTime },
+    { label: "地図",   href: "/map",       Icon: MdOutlineMap }
   ];
   return (
     <footer className={styles["ft"]}>
       <div className={styles["ft-blur"]}/>
       <nav className={styles["ft-main"]}>
         <ul className={styles["ft-list"]}>
-          {items.map(({ label, href, icon}) => {
+          {items.map(({ label, href, Icon}) => {
             const isActive = href === activePage;
             return (
               <li key={href}>
@@ -32,8 +29,8 @@ export default function Footer(){
                   onClick={() => setActivePage(href)}
                   className={`${styles["ft-item"]} ${isActive ? styles["ft-item-active"] : styles["ft-item-normal"]}`}
                 >
-                  {createIcon(icon)}
-                  <div className={styles["ft-label"]}> {label} </div>
+                  <Icon size={40} className={styles["ft-icon"]} />
+                  <span className={styles["ft-label"]}> {label} </span>
                 </Link>
               </li>
             );
