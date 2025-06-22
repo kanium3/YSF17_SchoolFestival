888a973a717b96fffc404cb1db8140223341c00e
ysy-255 <ysy-255@outlook.com>
2025-06-22 16:10:18 +0900

    フッターの改善

--- apps/web/app/compoent/footer.jsx
+++ apps/web/app/compoent/footer.jsx
@@ -1,42 +1,43 @@
+"use client"
+
+import { useState } from "react"
 import styles from "./footer.module.css"
 import Link from "next/link"
 import { MdHome, MdOutlineFastfood, MdWysiwyg, MdAccessTime, MdOutlineMap } from "react-icons/md"
 
+function createIcon(Icon) {
+  return <Icon size={40} className={styles["ft-icon"]} />;
+}
+
 export default function Footer(){
+  const [activePage, setActivePage] = useState(null);
+  const items = [
+    { label: "案内",   href: "/",          icon: MdHome },
+    { label: "食品",   href: "/dining",    icon: MdOutlineFastfood },
+    { label: "企画",   href: "/program",   icon: MdWysiwyg },
+    { label: "時間割", href: "/timetable", icon: MdAccessTime },
+    { label: "地図",   href: "/map",       icon: MdOutlineMap }
+  ];
   return (
-    <footer className={styles["ft-main"]}>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">
-              <MdHome size={40} color="black"/>
-              <div>案内</div>
-            </Link>
-          </li>
-          <li>
-            <Link href="/dining">
-              <MdOutlineFastfood size={40} color="black"/>
-              <div>食品</div>
-            </Link>
-          </li>
-          <li>
-            <Link href="/program">
-              <MdWysiwyg size={40} color="black"/>
-              <div>企画</div>
-            </Link>
-          </li>
-          <li>
-            <Link href="/timetable">
-              <MdAccessTime size={40} color="black"/>
-              <div>時間割</div>
-            </Link>
-          </li>
-          <li>
-            <Link href="/map">
-              <MdOutlineMap size={40} color="black"/>
-              <div>地図</div>
-            </Link>
-          </li>
+    <footer className={styles["ft"]}>
+      <div className={styles["ft-blur"]}/>
+      <nav className={styles["ft-main"]}>
+        <ul className={styles["ft-list"]}>
+          {items.map(({ label, href, icon}) => {
+            const isActive = href === activePage;
+            return (
+              <li key={href}>
+                <Link
+                  href={href}
+                  onClick={() => setActivePage(href)}
+                  className={`${styles["ft-item"]} ${isActive ? styles["ft-item-active"] : styles["ft-item-normal"]}`}
+                >
+                  {createIcon(icon)}
+                  <div className={styles["ft-label"]}> {label} </div>
+                </Link>
+              </li>
+            );
+          })}
         </ul>
       </nav>
     </footer>
