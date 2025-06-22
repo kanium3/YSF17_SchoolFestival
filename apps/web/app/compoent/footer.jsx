8630f099fa420bd40338171022dce98fd4f6e15d
ysy-255 <ysy-255@outlook.com>
2025-06-22 17:08:12 +0900

    activePageの初期化及び更新をいい感じに(自分の中ではかなり最適に近いと思っている)

--- apps/web/app/compoent/footer.jsx
+++ apps/web/app/compoent/footer.jsx
@@ -1,13 +1,29 @@
 "use client"
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import styles from "./footer.module.css"
 import Link from "next/link"
 import { MdHome, MdOutlineFastfood, MdWysiwyg, MdAccessTime, MdOutlineMap } from "react-icons/md"
-
+import { usePathname } from "next/navigation"
 
 export default function Footer(){
-  const [activePage, setActivePage] = useState(null);
+
+  /*
+    activePathの初期化・更新について:
+    pathname = "/a/b/c" とすると、
+    最初 (レンダリング): b以下があろうと /a に初期化
+    その後 (ページ遷移): b以下がない場合に限り /a に更新
+  */
+
+  const pathname = usePathname();
+  const splitPath = pathname.split('/');
+  const [activePage, setActivePage] = useState('/' + splitPath[1]);
+  useEffect(() => {
+    if(splitPath.length == 2){
+      setActivePage('/' + splitPath[1]);
+    }
+  }, [pathname]);
+
   const items = [
     { label: "案内",   href: "/",          Icon: MdHome },
     { label: "食品",   href: "/dining",    Icon: MdOutlineFastfood },
