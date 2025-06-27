cef18aceee99c292f16e5200c079dead04bc7cdf
kanium3 <dev@kanium.me>
2025-06-28 08:57:04 +0900

    turbopackからrspackへ移行 turbopackではSVGやファイルの生importといったwebpackプラグインがそのまま動作しないためにwebpackのrust版であるrspackを使用します

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,9 +1,39 @@
+import withRspack from 'next-rspack'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
+  webpack(config) {
+    // These settings are from https://react-svgr.com/docs/next/
+    const fileLoaderRule = config.module.rules.find(rule =>
+      rule.test?.test?.('.svg'),
+    )
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/, /raw/] },
+        use: ['@svgr/webpack'],
+      },
+      {
+        resourceQuery: /raw/, // *.*?raw
+        type: 'asset/source',
+      },
+    )
+
+    fileLoaderRule.exclude = /\.svg$/i
+
+    return config
+  },
 }
 
-export default nextConfig
+export default withRspack(nextConfig)
