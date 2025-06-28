7385bcd0dd3ee8fd7b07deb05fd952dcacaeb306
OR-11 <93813653+OR-11@users.noreply.github.com>
2025-06-28 21:49:06 +0900

    アレルギー表ページの追加

---
+++ apps/web/app/dining/foodSales/allergyTable/page.jsx
@@ -0,0 +1,15 @@
+// import styles from './page.module.css'
+// import { solveBasePath } from '@/app/lib/index.js'
+// import Image from 'next/image'
+// import Link from 'next/link'
+
+export const metadata = {
+  title: '蒼煌祭17th非公式ページ｜アレルギー表',
+  description: '蒼煌祭17thの食販のアレルギー表のページです',
+}
+
+export default function AllergyTable() {
+  return (
+    <></>
+  )
+}
