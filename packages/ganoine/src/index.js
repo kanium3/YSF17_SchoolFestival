471759f049159ef5631bb52e930bbe8a5fe07ed4
ysy-255 <ysy-255@outlook.com>
2025-07-11 18:41:38 +0900

    伴う変更

--- packages/ganoine/src/index.js
+++ packages/ganoine/src/index.js
@@ -1,5 +1,4 @@
 export { Callout } from './callout'
-export { BackButton } from './back-button'
 
 // react-ariaで提供されるパスを制御可能なものにするためエクスポートします
 export { RouterProvider } from 'react-aria-components'
