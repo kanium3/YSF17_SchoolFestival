75945691fa2602b95275add443d336a496468019
OR-11 <93813653+OR-11@users.noreply.github.com>
2025-07-13 16:34:50 +0900

    Applied ESlint rule

--- apps/web/app/timetable/timetableElements/timetable-column-heading.jsx
+++ apps/web/app/timetable/timetableElements/timetable-column-heading.jsx
@@ -1,20 +1,24 @@
 import styles from './timetable-column-heading-styles.module.css'
-import { MdKeyboardDoubleArrowRight } from "react-icons/md";
+import { MdKeyboardDoubleArrowRight } from 'react-icons/md'
 
 export function Column_heading({ programsInfo, index }) {
   const open_detail = () => {
-    const column = document.querySelector(`#program_${index}`);
-    column.classList.toggle('closed_inTimetable');
-    column.scrollIntoView({ behavior: 'smooth' });
+    const column = document.querySelector(`#program_${index}`)
+    column.classList.toggle('closed_inTimetable')
+    column.scrollIntoView({ behavior: 'smooth' })
   }
   return (
     <div className={styles.headingBoard}>
       <p className={styles.teamName}>{programsInfo.team}</p>
       <p className={styles.summary}>{programsInfo.summary}</p>
       <p className={styles.extraInfo}>
-        #{programsInfo.hashtag}
+        #
+        {programsInfo.hashtag}
       </p>
-      <button className={`${styles.expandButton} open_button`} onClick={open_detail}>詳細<MdKeyboardDoubleArrowRight className={styles.icons} /></button>
+      <button className={`${styles.expandButton} open_button`} onClick={open_detail}>
+        詳細
+        <MdKeyboardDoubleArrowRight className={styles.icons} />
+      </button>
     </div>
   )
 }
