742c1d279c51a38c5ca19d6226d180047db1b9a5
kanium3 <dev@kanium.me>
2025-07-20 16:20:15 +0900

    ビルド時に色々出せるようにした
    バンドルサイズとかライセンス

--- apps/web/app/lib/index.js
+++ apps/web/app/lib/index.js
@@ -1,16 +1,14 @@
-import nextConfig from '@/next.config.js'
-
 export * from './svg-controller.js'
 export * from '@latimeria/core'
 
 /**
  * URLパスを取得する。
- * `NODE_ENV`が有効時、`next.config.js`の`basePath`を参照してそこから始まるパスを返します。開発時は`src`をそのまま返します。
+ * `NODE_ENV`が有効時、`/YSF17_SchoolFestival`から始まるパスを返します。開発時は`src`をそのまま返します。
  * @param {string} source
  * @return {string}
  */
 export function solveBasePath(source) {
   return process.env.NODE_ENV === 'production'
-    ? `${nextConfig.basePath}${source}`
+    ? `/YSF17_SchoolFestival${source}`
     : source
 }
