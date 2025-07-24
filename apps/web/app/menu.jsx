dfa20cc701dfc708e6e75a4f9d048d16595e034e
ysy-255 <ysy-255@outlook.com>
2025-07-24 20:41:37 +0900

    ロードしてからMenuを表示する

--- apps/web/app/menu.jsx
+++ apps/web/app/menu.jsx
@@ -1,14 +1,23 @@
 'use client'
 
-import { isMobile, isDesktop } from 'react-device-detect'
+import { isMobile } from 'react-device-detect'
 import Header from '@/app/compoent/header'
 import NaviFooter from '@/app/compoent/global/navi-footer'
 import { ClientProvider } from '@/app/provider.jsx'
+import { useEffect, useState } from 'react'
 
 export default function Menu() {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return
+
   return (
     <ClientProvider>
-      {isMobile ? <NaviFooter /> : (isDesktop ? <Header /> : undefined) }
+      {isMobile ? <NaviFooter /> : <Header /> }
     </ClientProvider>
   )
 }
