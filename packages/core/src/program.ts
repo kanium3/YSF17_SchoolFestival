6cbe3b3d2201cb2df57057af7dab2a309711f017
OR-11 <93813653+OR-11@users.noreply.github.com>
2025-07-25 15:35:13 +0900

    やっぱさっきの無しで

--- packages/core/src/program.ts
+++ packages/core/src/program.ts
@@ -118,41 +118,19 @@ export class Programs {
    * @param tags
    * @param is_complete すべてのタグが完全に一致したもののみを返すか
    */
-  matchPrograms(tags: Tags, _is_complete: boolean = false): Programs {
-    let matchedProgramsResult = new Programs([])
-    let firstTime = true
-    for (const tag of tags) {
-      let matchedPrograms = new Programs([])
-      if (firstTime) {
-        for (const program of this.programs) {
-          const programTags = program.tags
-          // if (is_complete && tags.isSupersetOf(programTags)) {
-          //  matchedPrograms.programs.add(program)
-          //  continue
-          // }
-          if (programTags.has(tag)) { // (!tags.isDisjointFrom(programTags)) {
-            matchedPrograms.programs.add(program)
-          }
-        }
-        firstTime = false
+  matchPrograms(tags: Tags, is_complete: boolean = false): Programs {
+    const matchedPrograms = new Programs([])
+    for (const program of this.programs) {
+      const programTags = program.tags
+      if (is_complete && tags.isSupersetOf(programTags)) {
+        matchedPrograms.programs.add(program)
+        continue
       }
-      else {
-        for (const program of matchedProgramsResult.programs) {
-          const programTags = program.tags
-          // if (is_complete && tags.isSupersetOf(programTags)) {
-          //  matchedPrograms.programs.add(program)
-          //  continue
-          // }
-          if (programTags.has(tag)) { // (!tags.isDisjointFrom(programTags)) {
-            matchedPrograms.programs.add(program)
-          }
-        }
+      if (!tags.isDisjointFrom(programTags)) {
+        matchedPrograms.programs.add(program)
       }
-      matchedProgramsResult = matchedPrograms
-      matchedPrograms = new Programs([])
     }
-
-    return matchedProgramsResult
+    return matchedPrograms
   }
 
   /**
