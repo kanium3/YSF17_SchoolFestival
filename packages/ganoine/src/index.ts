73cec159df3db4ba654e4a85825d08bfa97c9ec9
ameirok <151885371+proirok@users.noreply.github.com>
2025-07-25 07:39:47 +0000

    storybook向けのものを作りました。これからさらに改良予定。

--- packages/ganoine/src/index.ts
+++ packages/ganoine/src/index.ts
@@ -1,6 +1,8 @@
 export { Callout } from './callout'
 export { Button } from './button'
+export { ProgramPopup } from './popup'
 export type { ButtonProperties, ButtonKinds } from './button'
+export type { PopupProperties, PopupKinds } from './popup'
 
 // react-ariaで提供されるパスを制御可能なものにするためエクスポートします
 export { RouterProvider } from 'react-aria-components'
--- packages/ganoine/src/popup.tsx
+++ packages/ganoine/src/popup.tsx
@@ -1,17 +1,67 @@
+'use client'
+
+import styles from './popup.module.css'
+import type { ReactNode } from 'react'
+import { useState } from 'react'
+import { DialogTrigger, Dialog, Modal, ModalOverlay } from 'react-aria-components'
+import { Button } from './button'
+
+export type PopupKinds = 'default'
+
+export type PopupProperties = {
+  children: ReactNode
+  hasOpenButton?: boolean
+  kind?: PopupKinds
+  popwidth?: number
+  popheight?: number
+}
+
 /**
  * ## Ganoineのポップアップコンポーネント
- * ``プロパティを指定することで、ポップアップを作成することができます。
+ * `ProgramPopup`を使うことでポップアップを作成することができます。
  *
- * 例えば、``とすることで、。
- * デフォルトでは、`react-aria-components`の`Modal`コンポーネントが使用されます。
+ * 例えばミニページやエラーなどを表示させるポップアップを作れます。
  *
- * ``プロパティを指定することで、ボタンの種類を変更できます。
+ * `hasOpenButton`プロパティに`true`を指定することで、開くためのボタンをつけることができます。
  *
- * ``は企画ページ用のポップアップ、``はその他の子要素を取れるポップアップとして使用されます。
+ * `PopupKind`プロパティに種類を指定することで子要素の中身を指定することもできます。子要素を自由に取りたい場合は`default`を使用します。
+ * (現在`PopupKind`に指定できるのは`default`のみです。)
  *
- * ``
  **/
 
-export function ProgramPopup(){
+export function ProgramPopup(properties: PopupProperties): ReactNode {
+  const popheight = properties.popheight ?? window.innerHeight - 144
+  const popwidth = properties.popwidth ?? widthAdjust(window.innerWidth - 24)
+  const screenSize = {
+    '--popwidth': `${popwidth}px`,
+    '--popheight': `${popheight}px`,
+  }
+  // const [isDisplayPage, setDisplayPage] = useState(false)
+  return (
+    <DialogTrigger>
+      { properties.hasOpenButton
+        && (
+          <Button>
+            <p>ポップアップを開く</p>
+          </Button>
+        )}
+      <ModalOverlay isDismissable className={styles.backdrop}>
+        <Modal>
+          <Dialog>
+            <div className={styles.main} style={screenSize as React.CSSProperties}>
+              {properties.children}
+            </div>
+          </Dialog>
+        </Modal>
+      </ModalOverlay>
+    </DialogTrigger>
+  )
+}
 
+function widthAdjust(width: number) {
+  return Math.min(width * 0.85, width * 0.5 + 200)
+}
+
+const handleContentClick = (event) => {
+  event.stopPropagation()
 }
