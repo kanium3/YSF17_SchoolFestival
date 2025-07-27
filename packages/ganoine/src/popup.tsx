2894df02417d65c9d6203bbadc87ee5398d77334
ameirok <151885371+proirok@users.noreply.github.com>
2025-07-27 13:36:53 +0900

    Update popup.tsx
    昨日の作業を全てコミットしてプルリクする

--- packages/ganoine/src/popup.tsx
+++ packages/ganoine/src/popup.tsx
@@ -2,7 +2,7 @@
 
 import styles from './popup.module.css'
 import type { ReactNode } from 'react'
-import { useState } from 'react'
+import { useModalContext } from './ModalContext'
 import { Dialog, Modal, ModalOverlay } from 'react-aria-components'
 import { Button } from './button'
 
@@ -10,24 +10,23 @@ export type PopupKinds = 'default'
 
 export type PopupProperties = {
   children: ReactNode
-  buttonChildren?: ReactNode
-  hasOpenButton?: boolean
-  kind?: PopupKinds
   popwidth?: number
   popheight?: number
 }
 
 /**
  * ## Ganoineのポップアップコンポーネント
- * `ProgramPopup`を使うことでポップアップを作成することができます。
- * Popupでは予約語であることが多いため、`ProgramPopup`となっていることに気をつけてください。
+ * `ProgramPopup`を使うことでreact-aria-componentsのModalを使用したポップアップを作成することができます。
  *
- * `hasOpenButton`プロパティに`true`を指定することで、開くためのボタンをつけることができます。
- * ボタンがない場合(例: react leafletの図形など)には、必ず親要素でコールバック関数が必要となります。
+ * このコンポーネントは必ず`<ModalProvider>`を親要素ことに取ることが必要となります。`<ModalProvider>`は`context`を用いたModalの開閉を認識するための関数です。
+ * `<ModalProvider>`は`@latimeria/ganoine`よりインポートできます。
  *
- * `PopupKind`プロパティに種類を指定することで子要素の中身を指定することもできます。子要素を自由に取りたい場合は`default`を使用します。
- * (現在`PopupKind`に指定できるのは`default`のみです。)
+ * `hasOpenButton`に`true`を指定することで、開くためのボタンをつけることができます。
+ * `false`を指定した場合は`handleClik`に`usePopup`の値を設定することが必要になります。設定した後は、クリックイベントを入れたいタグに`onClick`などで設定します。
+ * この`false`の場合にも対応させるため、`hasOpenButton`の状態にかかわらず必ず`<ModalProvider>`を必要とします。
  *
+ * `popheight`や`popwidth`に数値を代入することで、ポップアップの大きさを指定できます。デフォルトではPCでは画面サイズのおよそ0.6倍ほど、スマホでは画面サイズの0.85倍ほどになるように設定されています。
+ 
  **/
 
 export function ProgramPopup(properties: PopupProperties): ReactNode {
@@ -37,29 +36,20 @@ export function ProgramPopup(properties: PopupProperties): ReactNode {
     '--popwidth': `${popwidth}px`,
     '--popheight': `${popheight}px`,
   }
-  const [isOpen, setOpen] = useState(false)
+  const { isOpen, toggleModal} = useModalContext();
   return (
     <>
-      { properties.hasOpenButton
-        && (
-          <Button onPress={() => setOpen(true)}>
-            <p>ポップアップを開く</p>
-          </Button>
-        )}
-      { properties.buttonChildren
-        && (
-          <div>
-            {properties.buttonChildren}
-          </div>
-        )}
-      <ModalOverlay isDismissable isOpen={isOpen} onOpenChange={setOpen} className={styles.backdrop}>
+      <ModalOverlay isDismissable isOpen={isOpen} onOpenChange={toggleModal} className={styles.backdrop}>
         <Modal>
           <Dialog>
             <div className={styles.main} style={screenSize as React.CSSProperties}>
-              <Button onPress={() => setOpen(false)}>
+              <Button onPress={toggleModal}>
                 <p>✕</p>
               </Button>
               {properties.children}
+              <Button onPress = {toggleModal}>
+                <p>閉じる</p>
+              </Button>
             </div>
           </Dialog>
         </Modal>
