d54c81a8eb087838cbab1b222efe4774e98a7226
ysy-255 <ysy-255@outlook.com>
2025-07-30 00:50:54 +0900

    原型を作成

--- apps/web/app/compoent/map/bottom-sheet.jsx
+++ apps/web/app/compoent/map/bottom-sheet.jsx
@@ -1,18 +1,60 @@
-import styles from './bottom-sheet.module.css'
+'use client'
 
+import { useState, useEffect } from 'react'
+import { MdClose, MdUnfoldMore, MdUnfoldLess } from 'react-icons/md'
+
+import styles from './bottom-sheet.module.css'
+import ProgramSample from '@/app/program.mock.json'
+import { parseProgramsData, solveBasePath } from '@/app/lib/index.js'
+
+/**
+ * @param {string[]} ids
+ */
 export default function BottomSheet({ ids, onClose }) {
+  const [opened, setOpened] = useState(false)
+  const [selectedId, setSelectedId] = useState(ids ? ids[0] : undefined)
+  useEffect(() => {
+    setSelectedId(undefined)
+  }, [ids])
+  const programs = parseProgramsData(ProgramSample)
   return (
-    <div className={`${styles['bottom-sheet']} ${ids.length > 0 ? styles['open'] : ''}`}>
-      {ids && (
-        <>
-          <div className="bottom-sheet-header">
-            <button onClick={onClose}>閉じる</button>
+    <div className={`${styles['btst']} ${ids.length > 0 ? (opened ? styles['open'] : styles['small']) : styles['none']}`}>
+      <div className={styles['btst-header']}>
+        <div onClick={() => setOpened(!opened)} className={styles['btst-fold']}>
+          {opened
+            ? <MdUnfoldLess size={30} />
+            : <MdUnfoldMore size={30} />}
+        </div>
+        <div
+          onClick={() => {
+            onClose()
+            setOpened(false)
+          }}
+          className={styles['btst-close']}
+        >
+          <MdClose size={30} />
+        </div>
+      </div>
+      <div>
+        {ids.map((id) => {
+          const program = [...programs.iter()].find(program => program.id === id)
+          if (!program) return (
+            <></>
+          )
+          return (
+            <div onClick={() => setSelectedId(id)} key={id}>
+              {program.name}
+            </div>
+          )
+        })}
+      </div>
+      <div>
+        {selectedId && (
+          <div>
+            {[...programs.iter()].find(program => program.id === selectedId).prText}
           </div>
-          <div className="bottom-sheet-content">
-            <p>{ids.join(', ')}</p>
-          </div>
-        </>
-      )}
+        )}
+      </div>
     </div>
   )
 }
