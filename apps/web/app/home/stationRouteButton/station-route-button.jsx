243114645d78b84cab5361a3f64cf1d3fc356128
konryu-kin <165574816+konryu-kin@users.noreply.github.com>
2025-07-30 11:25:32 +0000

    Apply Eslint Change

--- apps/web/app/home/stationRouteButton/station-route-button.jsx
+++ apps/web/app/home/stationRouteButton/station-route-button.jsx
@@ -4,12 +4,12 @@ import styles from './station-route-button.module.css'
 
 //  何に分類されるか分からないので一旦ここで実装します
 export default function RouteToStationButton({ link }) {
-    return (
-        <Link href={link} target="_blank">
-            <div className={styles.button}>
-                経路
-                <MdOutlineOpenInNew className={styles.icon} />
-            </div>
-        </Link>
-    )
+  return (
+    <Link href={link} target="_blank">
+      <div className={styles.button}>
+        経路
+        <MdOutlineOpenInNew className={styles.icon} />
+      </div>
+    </Link>
+  )
 }
