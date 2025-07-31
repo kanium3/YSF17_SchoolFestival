765141ce05924b4ffa3909924766f1a989a52595
ysy-255 <ysy-255@outlook.com>
2025-07-31 22:06:15 +0900

    polygonフォルダの削除

--- apps/web/data/map/polygons/program-all.js
+++
@@ -1,9 +0,0 @@
-import programs from '../../../program.mock.json'
-
-export function programAll() {
-  return programs.map((item) => {
-    return {
-      teamId: item.id,
-      name: item.name }
-  })
-}
