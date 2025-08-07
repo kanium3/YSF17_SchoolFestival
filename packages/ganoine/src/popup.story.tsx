1e9e6e0db190b4260a08edf727eafb411a094748
ameirok <151885371+proirok@users.noreply.github.com>
2025-08-07 12:25:10 +0000

    ポップアップのエラーをいくつか修正

--- packages/ganoine/src/popup.story.tsx
+++ packages/ganoine/src/popup.story.tsx
@@ -9,22 +9,70 @@ export default {
   component: Popup,
 } as Meta<typeof Popup>
 
-export const ProgramPopup = {
-  render: () => (
-    <PopupProvider>
-      <ModalButton />
-      <Popup>
+export const Default = {
+  args: {
+    children: (
+      <>
         <div>
           <h1>ミニページのタイトル</h1>
-          <p>ポップアップ(モーダル)の例です。</p>
+          <p>全ての設定がデフォルトのポップアップ(モーダル)の例です。</p>
         </div>
         <PopupCloseButton />
-      </Popup>
+      </>
+    ),
+  },
+  render: args => (
+    <PopupProvider>
+      <ModalButton />
+      <Popup {...args} />
+    </PopupProvider>
+  ),
+} as StoryObj<typeof Popup>
+
+export const PopupLocked = {
+  args: {
+    isDismissable: false,
+    children: (
+      <>
+        <div>
+          <h1>クリックに反応しないポップアップ</h1>
+          <p>背景をクリックしても閉じないタイプのポップアップです。</p>
+        </div>
+        <PopupCloseButton />
+      </>
+    ),
+  },
+  render: args => (
+    <PopupProvider>
+      <ModalButton />
+      <Popup {...args} />
+    </PopupProvider>
+  ),
+} as StoryObj<typeof Popup>
+
+export const PopupSizefixed = {
+  args: {
+    popupHeight: 500,
+    popupWidth: 500,
+    children: (
+      <>
+        <div>
+          <h1>サイズ固定のポップアップ</h1>
+          <p>サイズを500✕500（単位：px）に固定したタイプのポップアップです。</p>
+        </div>
+        <PopupCloseButton />
+      </>
+    ),
+  },
+  render: args => (
+    <PopupProvider>
+      <ModalButton />
+      <Popup {...args} />
     </PopupProvider>
   ),
 } as StoryObj<typeof Popup>
 
 const ModalButton = () => {
-  const { toggleModal } = usePopup()
-  return <Button onPress={() => toggleModal()}>ポップアップを開くボタン</Button>
+  const { togglePopup } = usePopup()
+  return <Button onPress={() => togglePopup()}>ポップアップを開くボタン</Button>
 }
