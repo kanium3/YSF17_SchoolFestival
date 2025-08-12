8a0f2d86bd3d6ec5227041ee1edc1751952223f0
kanium3 <dev@kanium.me>
2025-08-12 21:47:26 +0900

    projectの形に持っていっている

--- apps/unoffcial/src/index.jsx
+++ apps/unoffcial/src/index.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
+import App from './app.jsx'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.querySelector('#root'))
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-);
+  </BrowserRouter>,
+)
