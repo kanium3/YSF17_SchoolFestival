56fd53bed0272218fe87969480a8dbac85797b54
kanium3 <dev@kanium.me>
2026-03-18 13:02:58 +0900

    Delete eslint configs

--- packages/ganoine/eslint.config.mjs
+++
@@ -1,6 +0,0 @@
-import { config } from '@latimeria/eslint-config/with-react'
-
-export default [
-  ...config,
-  { ignores: ['dist/', 'generated/'] },
-]
--- packages/scripts/eslint.config.js
+++
@@ -1,6 +0,0 @@
-import { config } from '@latimeria/eslint-config/base'
-
-export default [
-  ...config,
-  { ignores: ['dist/', '*/generated/*'] },
-]
